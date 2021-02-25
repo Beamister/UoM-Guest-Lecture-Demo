89fe347bc25d4a42f7e42097b8ea9702e1af749f
Luke <luke.beamish@peak.ai>
2021-02-25 14:19:13 +0000

    Updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_pN2rk7uSY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1neief0ujn0ha9n3sj039fod15', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
